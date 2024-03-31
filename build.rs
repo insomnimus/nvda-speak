95e623e120811eb1891e67fd37df2f70383287f7
Taylan Gökkaya <insomnimus@proton.me>
2024-03-31 15:29:54 +0300

    fix: fix build script target detection

--- build.rs
+++ build.rs
@@ -1,19 +1,14 @@
 use std::env;
 
 fn main() {
-	if !cfg!(windows) {
-		panic!("this program is for windows");
-	}
 	println!("cargo:rerun-if-changed=ffi");
 
-	let (dir, lib) = if cfg!(target_arch = "x86_64") {
-		("ffi/x64", "nvdaControllerClient64")
-	} else if cfg!(target_arch = "x86") {
-		("ffi/x86", "nvdaControllerClient32")
-	} else if cfg!(target_arch = "aarch64") {
-		("ffi/arm64", "nvdaControllerClient32")
-	} else {
-		panic!("the target architecture is not supported");
+	let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+	let (dir, lib) = match target_arch.as_str() {
+		"x86_64" => ("ffi/x64", "nvdaControllerClient64"),
+		"x86" => ("ffi/x86", "nvdaControllerClient32"),
+		"aarch64" => ("ffi/arm64", "nvdaControllerClient32"),
+		_ => panic!("the target architecture {target_arch} is not supported"),
 	};
 
 	let p = env::current_dir().unwrap();
